0f8369807f8467f7c440d7519834e25f9468415a
Dhanuka Rathnayaka <lohandanuka@gmail.com>
2025-09-24 18:14:42 +0530

    centralized the spacex endpoints

---
+++ spacex-explorer/src/api/spacex.ts
@@ -0,0 +1,8 @@
+const BASE = 'https://api.spacexdata.com/v4'
+
+export const enpoints = {
+    launches: `${BASE}/launches`,
+    rockets: `${BASE}/rockets`,
+    launchpads: `${BASE}/launchpads`,
+    payloads: `${BASE}/payloads`,
+}
