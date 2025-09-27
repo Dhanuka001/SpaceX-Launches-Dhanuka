bee1de3ed8b56dff40c3261f711a8c98ac414708
Dhanuka Rathnayaka <lohandanuka@gmail.com>
2025-09-27 14:07:53 +0530

    feat(ui): add empty state message for no matching launches in LaunchList

--- spacex-explorer/src/components/LaunchList.tsx
+++ spacex-explorer/src/components/LaunchList.tsx
@@ -34,29 +34,34 @@ export default function LaunchList() {
                 </div>
             ) : (
                 <>
-                <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mt-6">
-                    {items.map(l => (
-                        <LaunchCard 
-                            key={l.id}
-                            launch={l}
-                            isFavorite={favSet.has(l.id)}
-                            onToggleFavorite={toggleFav}
-                        />
-                    ))}
-                </div>
-
-                <nav aria-label="pagination" className="flex gap-2 justify-center mt-4  ">
+                {items.length === 0 ? (
+                    <div className="text-center py-12 text-gray-500">
+                        <p>Opps! No Launches Found</p>
+                        <p>Try adjusting your filters or search query.</p>
+                    </div>
+                ) : (
+                    <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mt-6">
+                        {items.map(l => (
+                            <LaunchCard 
+                                key={l.id}
+                                launch={l}
+                                isFavorite={favSet.has(l.id)}
+                                onToggleFavorite={toggleFav}
+                            />
+                        ))}
+                    </div>
+                )}
+               
+               {items.length > 0 && (
+                     <nav aria-label="pagination" className="flex gap-2 justify-center mt-4  ">
                     <button disabled={page===1} className="bg-gray-50 rounded-md px-2 py-1 disabled:opacity-50 hover:bg-black hover:text-white" onClick={() => setPage(1)}>First</button>
                     <button disabled={page===1} className="bg-gray-50 rounded-md px-2 py-1 disabled:opacity-50 hover:bg-black hover:text-white" onClick={() => setPage(1)}>← Back</button>
                     <span className="px-2 py-1">Page {page} / {totalPages}</span>
                     <button disabled={page===totalPages} className="bg-gray-100 rounded-md px-2 py-1 disabled:opacity-50 hover:bg-black hover:text-white" onClick={() => setPage(page+1)}>Next →</button>
                     <button disabled={page===totalPages} className="bg-gray-100 rounded-md px-2 py-1 disabled:opacity-50 hover:bg-black hover:text-white" onClick={() => setPage(totalPages)}>Last</button>
-                    
-                    
-
                 </nav>
-             
-                </>
+               )}  
+             </>
             )}
         </section>
     )
