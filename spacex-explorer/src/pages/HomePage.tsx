0249bb1df6b6eb901df03a605470a1369ee13384
Dhanuka Rathnayaka <lohandanuka@gmail.com>
2025-09-27 12:22:41 +0530

    style(page): home styles update

--- spacex-explorer/src/pages/HomePage.tsx
+++ spacex-explorer/src/pages/HomePage.tsx
@@ -3,7 +3,14 @@ import LaunchList from "../components/LaunchList"
 export default function HomePage() {
     return (
         <div>
-            <h1>SpaceX Launches</h1>
+            <section className="text-center py-10">
+                 <h1 className="text-4xl font-bold mb-3 animate-pulse tracking-tight">SpaceX Launches</h1>
+                 <p className="text-lg text-gray-600 max-w-2xl mx-auto">
+                    Discover all SpaceX launches — past, present, and future.
+                    Filter by year, mission success, and mark your favorites to follow them easily.
+                 </p>
+            </section>
+           
             <LaunchList/>
         </div>
     )
