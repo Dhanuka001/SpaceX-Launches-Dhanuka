da337befbd56abb8a2b732b67fbd7764d85f16fc
Dhanuka Rathnayaka <lohandanuka@gmail.com>
2025-09-27 11:30:08 +0530

    feat(pages): integrate LaunchList into HomePage

--- spacex-explorer/src/pages/HomePage.tsx
+++ spacex-explorer/src/pages/HomePage.tsx
@@ -1,3 +1,10 @@
+import LaunchList from "../components/LaunchList"
+
 export default function HomePage() {
-    return <div>Home</div>
+    return (
+        <div>
+            <h1>SpaceX Launches</h1>
+            <LaunchList/>
+        </div>
+    )
 }
